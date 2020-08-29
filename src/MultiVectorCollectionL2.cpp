6121759e6377765b6733d553abcf7ae26ceec0a2
Abner <abner199709@gmail.com>
2020-08-29 17:30:45 +0800

    add init l2 code

--- src/MultiVectorCollectionL2.cpp
+++ src/MultiVectorCollectionL2.cpp
@@ -1,8 +1,50 @@
 #include "MultiVectorCollectionL2.h"
 
+
 namespace milvus {
 namespace multivector {
 
+Status
+MultiVectorCollectionL2::CreateCollection(const std::vector<int64_t> &dimensions,
+                                          const std::vector<int64_t> &index_file_sizes) {
+    return Status::OK();
+
+}
+
+Status
+MultiVectorCollectionL2::DropCollection() {
+    return Status::OK();
+}
+
+Status
+MultiVectorCollectionL2::Insert(const std::vector<milvus::multivector::RowEntity> &entity_arrays,
+                                std::vector<int64_t> &id_arrays) {
+    return Status::OK();
+}
+
+Status
+MultiVectorCollectionL2::Delete(const std::vector<int64_t> &id_arrays) {
+    return Status::OK();
+}
+
+Status
+MultiVectorCollectionL2::CreateIndex(milvus::IndexType index_type, const std::string &extra_params) {
+    return Status::OK();
+}
+
+
+Status
+MultiVectorCollectionL2::DropIndex() {
+    return Status::OK();
+}
+
+Status
+MultiVectorCollectionL2::Search(const std::vector<float> &weight,
+                                const std::vector<RowEntity> &entity_array,
+                                int64_t topk, const std::string &extra_params,
+                                milvus::TopKQueryResult &topk_query_results) {
+    return Status::OK();
+}
 
 } // namespace multivector
 } // namespace milvus
