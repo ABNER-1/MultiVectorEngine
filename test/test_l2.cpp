ec554269721677478b266c9e7287ad928bf126ea
abner <yuxing.yuan@zilliz.com>
2020-09-03 19:57:57 +0800

    stash read data from file

--- test/test_l2.cpp
+++ test/test_l2.cpp
@@ -12,19 +12,19 @@ main() {
     auto engine = std::make_shared<MultiVectorEngine>(ip, port);
 
     testIndexType(engine, milvus::IndexType::FLAT, {{"nlist", 1024}}, {{"nprobe", 20}}, milvus::MetricType::L2);
-    testIndexType(engine, milvus::IndexType::IVFFLAT, {{"nlist", 1024}}, {{"nprobe", 20}}, milvus::MetricType::L2);
-    testIndexType(engine, milvus::IndexType::IVFSQ8, {{"nlist", 1024}}, {{"nprobe", 20}}, milvus::MetricType::L2);
-    testIndexType(engine, milvus::IndexType::IVFPQ,
-                  {{"nlist", 1024}, {"m", 16}},
-                  {{"nprobe", 20}},
-                  milvus::MetricType::L2);
-    testIndexType(engine, milvus::IndexType::RNSG,
-                  {{"search_length", 45}, {"out_degree", 50}, {"candidate_pool_size", 300}, {"knng", 100}},
-                  {{"search_length", 100}}, milvus::MetricType::L2);
-    testIndexType(engine, milvus::IndexType::HNSW,
-                  {{"M", 16}, {"efConstruction", 500}},
-                  {{"ef", 200}}, milvus::MetricType::L2);
-    testIndexType(engine, milvus::IndexType::ANNOY,
-                  {{"n_trees", 8}},
-                  {{"search_k", -1}}, milvus::MetricType::L2);
+//    testIndexType(engine, milvus::IndexType::IVFFLAT, {{"nlist", 1024}}, {{"nprobe", 20}}, milvus::MetricType::L2);
+//    testIndexType(engine, milvus::IndexType::IVFSQ8, {{"nlist", 1024}}, {{"nprobe", 20}}, milvus::MetricType::L2);
+//    testIndexType(engine, milvus::IndexType::IVFPQ,
+//                  {{"nlist", 1024}, {"m", 16}},
+//                  {{"nprobe", 20}},
+//                  milvus::MetricType::L2);
+//    testIndexType(engine, milvus::IndexType::RNSG,
+//                  {{"search_length", 45}, {"out_degree", 50}, {"candidate_pool_size", 300}, {"knng", 100}},
+//                  {{"search_length", 100}}, milvus::MetricType::L2);
+//    testIndexType(engine, milvus::IndexType::HNSW,
+//                  {{"M", 16}, {"efConstruction", 500}},
+//                  {{"ef", 200}}, milvus::MetricType::L2);
+//    testIndexType(engine, milvus::IndexType::ANNOY,
+//                  {{"n_trees", 8}},
+//                  {{"search_k", -1}}, milvus::MetricType::L2);
 }
